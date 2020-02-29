4ba4c70d3ce0198f7c405ec04380ddb76b6251da
Matt Stanford <mds6058@gmail.com>
2020-02-29 13:14:09 -0500

    Initial Commit

---
+++ PterodactylLib/PterodactylLib.h
@@ -0,0 +1,19 @@
+//
+//  PterodactylLib.h
+//  PterodactylLib
+//
+//  Created by Matt Stanford on 2/29/20.
+//  Copyright © 2020 Matt Stanford. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PterodactylLib.
+FOUNDATION_EXPORT double PterodactylLibVersionNumber;
+
+//! Project version string for PterodactylLib.
+FOUNDATION_EXPORT const unsigned char PterodactylLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PterodactylLib/PublicHeader.h>
+
+
