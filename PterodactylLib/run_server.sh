a38584aaf31aa3403c5358f9920c74699a814f95
Matt Stanford <mds6058@gmail.com>
2020-03-22 17:15:06 -0400

    Copy pterodactyl server binary to PterodactylLib target when the server finishes its build

---
+++ PterodactylLib/run_server.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#  run_server.sh
+#  PterodactylLib
+#
+#  Created by Matt Stanford on 3/22/20.
+#  Copyright © 2020 Matt Stanford. All rights reserved.
+
+scriptLocation=`dirName $0`
+appLocation=`find $scriptLocation -name "PterodactylServer.app" | head -n 1`
+
+open -a $appLocation
+
+exit 0
+
+
